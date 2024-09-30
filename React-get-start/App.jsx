9991994b92e601d2b0dcd7aeafaf50c2662847fd
See2016 <devsee001@gmail.com>
2024-09-30 17:02:47 +0700

    last update form topic, refactoring code

--- React-get-start/App.jsx
+++ React-get-start/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import ClassBase from './components/class-component'
 import ContexButtonComponent from './components/context-concept/button'
 import ContextTextComponent from './components/context-concept/text'
+import FormComponent from './components/form'
 import FunctionComponent from './components/function-component'
 import ProductList from './components/products/index'
 import UseReducerExample from './components/use-reducer-example'
@@ -13,16 +14,18 @@ const productData = ['p1', 'p2', 'p3']
 function App() {
   return (
     <div>
-        <h1>Hello</h1>
-        {/* <ClassBase /> */}
-        {/* <FunctionComponent /> */}
-        {/* <ProductList ProductData={productData} name="See" city="PSU" /> */}
-        {/* <Users /> */}
+      <h1>Hello</h1>
+      {/* <ClassBase /> */}
+      {/* <FunctionComponent /> */}
+      {/* <ProductList ProductData={productData} name="See" city="PSU" /> */}
+      {/* <Users /> */}
 
-        {/* <ContexButtonComponent />
-        <ContextTextComponent /> */}
+      {/* <ContexButtonComponent />
+            <ContextTextComponent /> */}
+      {/* <UseReducerExample /> */}
 
-        <UseReducerExample />
+      {/* Form */}
+      <FormComponent />
     </div>
   )
 }
